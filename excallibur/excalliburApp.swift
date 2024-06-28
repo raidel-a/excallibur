815e0a5e58cb80a8a3dddfb19d8ef94abf0225fd
raidel-a <git@raidel.dev>
2024-06-28 01:53:40 -0400

    Initial Commit

---
+++ excallibur/excalliburApp.swift
@@ -0,0 +1,17 @@
+//
+//  excalliburApp.swift
+//  excallibur
+//
+//  Created by Raidel Almeida on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct excalliburApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ excalliburTests/excalliburTests.swift
@@ -0,0 +1,17 @@
+//
+//  excalliburTests.swift
+//  excalliburTests
+//
+//  Created by Raidel Almeida on 6/28/24.
+//
+
+import Testing
+@testable import excallibur
+
+struct excalliburTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
