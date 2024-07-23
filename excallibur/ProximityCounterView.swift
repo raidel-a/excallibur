e0a705bab598a66aecab2e3a1572cf659cf42a20
raidel-a <git@raidel.dev>
2024-07-22 23:54:17 -0400

    -slide to save workout add in push-up view

--- excallibur/ProximityCounterView.swift
+++ excallibur/ProximityCounterView.swift
@@ -10,229 +10,255 @@ import Combine
 import SwiftUI
 import UIKit
 
+// MARK: - ProximityDetectorView
+
 struct ProximityDetectorView: View {
-    @StateObject private var viewModel = ProximityDetectorViewModel()
-    @Environment(\.modelContext) private var modelContext
-    
-    var body: some View {
-        VStack {
-            Text("Proximity: \(viewModel.proximityState ? "Near" : "Far")")
-                .font(.title)
-                .padding()
-            Spacer()
-            HStack {
-                Button(action: {
-                    viewModel.updateCount(0, 1, false)
-                }) {
-                    Image(systemName: "minus.circle.fill")
-                        .font(.largeTitle)
-                        .foregroundColor(.orange)
-                }
-                
-                Text("Count: \(viewModel.objectCount)")
-                    .font(.largeTitle)
-                    .padding()
-                
-                Button(action: {
-                    viewModel.updateCount(1, 0, false)
-                }) {
-                    Image(systemName: "plus.circle.fill")
-                        .font(.largeTitle)
-                        .foregroundColor(.yellow)
-                }
-            }
-            
-            Text("Time: \(viewModel.formattedTime)")
-                .font(.title2)
-                .padding()
-            
-            HStack {
-                Button(action: {
-                    viewModel.updateCount(0, 0, true)
-                }) {
-                    Text("Reset Count")
-                }
-                .padding()
-                .background(.teal)
-                .cornerRadius(10)
-                
-                Button(action: viewModel.toggleTimer) {
-                    Text(viewModel.isTimerRunning ? "Pause" : "Start")
-                }
-//                .frame(minWidth: 0, maxWidth: 200)
-                .padding()
-                .background(.mint)
-                .cornerRadius(10)
-                
-                Button(action: viewModel.resetTimer) {
-                    Text("Reset Timer")
-                }
-                .padding()
-                .background(.teal)
-                .cornerRadius(10)
-            }
-            .padding()
-            .foregroundColor(.white)
-                
-            Spacer()
-            Button("Save Workout") {
-                viewModel.saveWorkout()
-            }
-            .padding()
-            .background(.cyan)
-            .foregroundColor(.white)
-            .cornerRadius(10)
-            .disabled(!viewModel.isTimerRunning && viewModel.objectCount == 0)
-        }
-        .navigationTitle("Push-up Tracker")
-        .onAppear {
-            viewModel.startMonitoring()
-        }
-        .onDisappear {
-            viewModel.stopMonitoring()
-        }
-    }
+	// MARK: Internal
+
+	var body: some View {
+		VStack {
+			Text("Proximity: \(viewModel.proximityState ? "Near" : "Far")")
+				.font(.title)
+				.padding()
+			Spacer()
+
+			HStack {
+				Button(action: {
+					viewModel.updateCount(0, 1, false)
+				}) {
+					Image(systemName: "minus.circle.fill")
+						.font(.largeTitle)
+						.foregroundColor(.orange)
+				}
+
+				Text("\(viewModel.objectCount)")
+					.font(.custom("Avenir Next", size: 100))
+					.fontWeight(.bold)
+					.rotation3DEffect(.degrees(flipX), axis: (x: 1, y: 0, z: 0))
+					.animation(.default.delay(0), value: flipX)
+					.padding()
+
+				Button(action: {
+					viewModel.updateCount(1, 0, false)
+				}) {
+					Image(systemName: "plus.circle.fill")
+						.font(.largeTitle)
+						.foregroundColor(.yellow)
+				}
+			}
+
+			Text("Time: \(viewModel.formattedTime)")
+				.font(.title2)
+				.padding()
+
+			Button(action: viewModel.toggleTimer) {
+				Text(viewModel.isTimerRunning ? "Pause" : "Start")
+			}
+			.padding()
+			.foregroundColor(.white)
+			.background(.mint)
+			.cornerRadius(10)
+
+			HStack {
+				Button {
+					viewModel.updateCount(0, 0, true)
+					withAnimation(.bouncy) {
+						flipX = (flipX == .zero) ? 360 : .zero
+					}
+				} label: {
+					Text("Rest Count")
+				}
+				.padding()
+				.background(.teal)
+				.cornerRadius(10)
+				Button(action: viewModel.resetTimer) {
+					Text("Reset Timer")
+				}
+				.padding()
+				.background(.teal)
+				.cornerRadius(10)
+			}
+			.padding()
+			.foregroundColor(.white)
+
+//			Spacer()
+			MTSlide(
+				isDisabled: !viewModel.isTimerRunning && viewModel.objectCount == 0,
+				thumbnailTopBottomPadding: 5,
+				thumbnailLeadingTrailingPadding: 5,
+				text: "Save Workout",
+				iconName: "plus",
+				didReachEndAction: { slider in
+					viewModel.saveWorkout()
+					slider.resetState()
+				}
+			)
+			.frame(height: 60)
+			.padding()
+		}
+		.navigationTitle("Push-up Tracker")
+		.onAppear {
+			viewModel.startMonitoring()
+		}
+		.onDisappear {
+			viewModel.stopMonitoring()
+		}
+	}
+
+	// MARK: Private
+
+	@State private var flipAngle = Double.zero
+
+	@State private var flipX = Double.zero
+
+	@StateObject private var viewModel = ProximityDetectorViewModel()
+	@Environment(\.modelContext) private var modelContext
 }
+
+// MARK: - ProximityDetectorViewModel
 
 class ProximityDetectorViewModel: ObservableObject {
-    @Published var proximityState = false
-    @Published var objectCount = 0
-    @Published var formattedTime = "00:00:00"
-    @Published var isTimerRunning = false
-    @Published var elapsedTime: TimeInterval = 0
-    
-    @AppStorage("isHapticFeedbackEnabled") private var isHapticFeedbackEnabled = true
-    @AppStorage("isVoiceFeedbackEnabled") private var isVoiceFeedbackEnabled = true
-    @AppStorage("selectedVoiceType") private var selectedVoiceType = "en-US"
-    @AppStorage("voiceSpeed") private var voiceSpeed: Double = 0.6
-    
-    private let device = UIDevice.current
-    private let synthesizer = AVSpeechSynthesizer()
-    private var timer: Timer?
-    
-    private let dataSource: WorkoutDataSource
-    
-    init(dataSource: WorkoutDataSource = .shared) {
-        self.dataSource = dataSource
-    }
-    
-    func startMonitoring() {
-        device.isProximityMonitoringEnabled = true
-        
-        if device.isProximityMonitoringEnabled {
-            NotificationCenter.default.addObserver(self,
-                                                   selector: #selector(proximityChanged),
-                                                   name: UIDevice.proximityStateDidChangeNotification,
-                                                   object: device)
-        }
-    }
-    
-    func stopMonitoring() {
-        device.isProximityMonitoringEnabled = false
-        NotificationCenter.default.removeObserver(self)
-        stopTimer()
-    }
-    
-    @objc private func proximityChanged(_ notification: Notification) {
-        DispatchQueue.main.async {
-            self.proximityState = self.device.proximityState
-            if self.proximityState {
-                self.objectCount += 1
-                self.announceCount()
-            }
-        }
-    }
-    
-    private func announceCount() {
-        if isHapticFeedbackEnabled {
-            let impact = UIImpactFeedbackGenerator(style: .medium)
-            impact.impactOccurred()
-        }
-        
-        if isVoiceFeedbackEnabled {
-            let ssml = """
-            <speak>
-               \(objectCount)
-            </speak>
-            """
-            
-            let utterance = AVSpeechUtterance(ssmlRepresentation: ssml)
-            utterance!.voice = AVSpeechSynthesisVoice(language: selectedVoiceType)
-            utterance!.rate = Float(voiceSpeed)
-            
-            synthesizer.speak(utterance!)
-        }
-    }
+	// MARK: Lifecycle
 
-//    func resetCount() {
-//        objectCount = 0
-//        announceCount()
-//    }
-    
-    func updateCount(_ increment: Int?, _ decrement: Int?, _ reset: Bool?) {
-        if let increment = increment {
-            objectCount += increment
-//            announceCount()
-        }
-        
-        if let decrement = decrement, objectCount - decrement >= 0 {
-            objectCount -= decrement
-//            announceCount()
-        }
-        
-        if let reset = reset, reset {
-            objectCount = 0
-            announceCount()
-        }
-    }
-    
-    func toggleTimer() {
-        if isTimerRunning {
-            stopTimer()
-        } else {
-            startTimer()
-        }
-    }
-    
-    private func startTimer() {
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
-            self?.elapsedTime += 1
-            self?.updateFormattedTime()
-        }
-        isTimerRunning = true
-    }
-    
-    private func stopTimer() {
-        timer?.invalidate()
-        timer = nil
-        isTimerRunning = false
-    }
-    
-    func resetTimer() {
-        stopTimer()
-        elapsedTime = 0
-        updateFormattedTime()
-    }
-    
-    @MainActor func saveWorkout() {
-        let workout = WorkoutData(date: Date(), duration: elapsedTime, count: objectCount, type: "pushup")
-        dataSource.addWorkout(workout)
-        print("Workout saved successfully: \(workout)")
-        stopMonitoring()
-        resetTimer()
-        updateCount(0, 0, true)
-    }
-    
-    private func updateFormattedTime() {
-        let hours = Int(elapsedTime) / 3600
-        let minutes = (Int(elapsedTime) % 3600) / 60
-        let seconds = Int(elapsedTime) % 60
-        formattedTime = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
-    }
+	init(dataSource: WorkoutDataSource = .shared) {
+		self.dataSource = dataSource
+	}
+
+	// MARK: Internal
+
+	@Published var proximityState = false
+	@Published var objectCount = 0
+	@Published var formattedTime = "00:00"
+	@Published var isTimerRunning = false
+	@Published var elapsedTime: TimeInterval = 0
+
+	func startMonitoring() {
+		device.isProximityMonitoringEnabled = true
+
+		if device.isProximityMonitoringEnabled {
+			NotificationCenter.default.addObserver(self,
+			                                       selector: #selector(proximityChanged),
+			                                       name: UIDevice.proximityStateDidChangeNotification,
+			                                       object: device)
+		}
+	}
+
+	func stopMonitoring() {
+		device.isProximityMonitoringEnabled = false
+		NotificationCenter.default.removeObserver(self)
+		stopTimer()
+	}
+
+	func updateCount(_ increment: Int?, _ decrement: Int?, _ reset: Bool?) {
+		if let increment {
+			objectCount += increment
+			//            announceCount()
+		}
+
+		if let decrement, objectCount - decrement >= 0 {
+			objectCount -= decrement
+			//            announceCount()
+		}
+
+		if let reset, reset {
+			objectCount = 0
+			//            announceCount()
+		}
+	}
+
+	func toggleTimer() {
+		if isTimerRunning {
+			stopTimer()
+		} else {
+			startTimer()
+		}
+	}
+
+	func resetTimer() {
+		stopTimer()
+		elapsedTime = 0
+		updateFormattedTime()
+	}
+
+	@MainActor func saveWorkout() {
+		let workout = WorkoutData(date: Date(), duration: elapsedTime, count: objectCount, type: "pushup")
+		dataSource.addWorkout(workout)
+		print("Workout saved successfully: \(workout)")
+		stopMonitoring()
+		resetTimer()
+		updateCount(0, 0, true)
+	}
+
+	// MARK: Private
+
+	@AppStorage("isHapticFeedbackEnabled") private var isHapticFeedbackEnabled = true
+	@AppStorage("isVoiceFeedbackEnabled") private var isVoiceFeedbackEnabled = true
+	@AppStorage("selectedVoiceType") private var selectedVoiceType = "en-US"
+	@AppStorage("voiceSpeed") private var voiceSpeed: Double = 0.6
+
+	private let device = UIDevice.current
+	private let synthesizer = AVSpeechSynthesizer()
+	private var timer: Timer?
+
+	private let dataSource: WorkoutDataSource
+
+	@objc private func proximityChanged(_: Notification) {
+		DispatchQueue.main.async {
+			self.proximityState = self.device.proximityState
+			if self.proximityState {
+				self.objectCount += 1
+				self.announceCount()
+			}
+		}
+	}
+
+	private func announceCount() {
+		if isHapticFeedbackEnabled {
+			let impact = UIImpactFeedbackGenerator(style: .medium)
+			impact.impactOccurred()
+		}
+
+		if isVoiceFeedbackEnabled {
+			let ssml = """
+			<speak>
+			   \(objectCount)
+			</speak>
+			"""
+
+			let utterance = AVSpeechUtterance(ssmlRepresentation: ssml)
+			utterance!.voice = AVSpeechSynthesisVoice(language: selectedVoiceType)
+			utterance!.rate = Float(voiceSpeed)
+
+			synthesizer.speak(utterance!)
+		}
+	}
+
+	private func startTimer() {
+		timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { [weak self] _ in
+			self?.elapsedTime += 1
+			self?.updateFormattedTime()
+		}
+		isTimerRunning = true
+	}
+
+	private func stopTimer() {
+		timer?.invalidate()
+		timer = nil
+		isTimerRunning = false
+	}
+
+	private func updateFormattedTime() {
+		let hours = Int(elapsedTime) / 3600
+		let minutes = (Int(elapsedTime) % 3600) / 60
+		let seconds = Int(elapsedTime) % 60
+		formattedTime = String(format: "%02d:%02d:%02d", hours, minutes, seconds)
+	}
 }
 
+// MARK: - ProximityDetectorView_Previews
+
 struct ProximityDetectorView_Previews: PreviewProvider {
-    static var previews: some View {
-        ProximityDetectorView()
-    }
+	static var previews: some View {
+		ProximityDetectorView()
+	}
 }
---
+++ excallibur/Slider.swift
@@ -0,0 +1,166 @@
+//
+//  Slider.swift
+//  excallibur
+//
+//  Created by Raidel Almeida on 7/22/24.
+//
+
+//
+//  MTSlideToOpen-SwiftUI.swift
+//  MTSlideToOpen-SwiftUI
+//
+//  Created by Le Manh Tien on 6/8/19.
+//  Copyright © 2019 io.tienle. All rights reserved.
+//
+
+import SwiftUI
+
+struct MTSlide: View {
+	// MARK: Internal
+
+	// Public Property
+	var isDisabled: Bool = false
+	var sliderTopBottomPadding: CGFloat = 0
+	var thumbnailTopBottomPadding: CGFloat = 0
+	var thumbnailLeadingTrailingPadding: CGFloat = 0
+	var textLabelLeadingPadding: CGFloat = 0
+	var text: String = "Slide"
+	var textFont: Font = .system(size: 15)
+	var textColor = Color(.sRGB, red: 25.0 / 255, green: 155.0 / 255, blue: 215.0 / 255, opacity: 0.7)
+	var thumbnailColor = Color(.sRGB, red: 25.0 / 255, green: 155.0 / 255, blue: 215.0 / 255, opacity: 1)
+	var thumbnailBackgroundColor: Color = .clear
+	var sliderBackgroundColor = Color(.sRGB, red: 0.1, green: 0.64, blue: 0.84, opacity: 0.1)
+	var resetAnimation: Animation = .easeIn(duration: 0.3)
+	var iconName = ""
+	var iconColor: Color = .white
+	var didReachEndAction: ((MTSlide) -> Void)?
+
+	var body: some View {
+		GeometryReader { geometry in
+			setupView(geometry: geometry)
+		}
+	}
+
+	// MARK: Public Function
+
+	func resetState(_: Bool = true) {
+		draggableState = .ready
+	}
+
+	// MARK: Private
+
+	private enum DraggableState {
+		case ready
+		case dragging(offsetX: CGFloat, maxX: CGFloat)
+		case end(offsetX: CGFloat)
+
+		// MARK: Internal
+
+		var reachEnd: Bool {
+			switch self {
+			case .ready, .dragging:
+				false
+			case .end:
+				true
+			}
+		}
+
+		var isReady: Bool {
+			switch self {
+			case .dragging(_), .end:
+				false
+			case .ready:
+				true
+			}
+		}
+
+		var offsetX: CGFloat {
+			switch self {
+			case .ready:
+				0.0
+			case let .dragging((offsetX, _)):
+				offsetX
+			case let .end(offsetX):
+				offsetX
+			}
+		}
+
+		var textColorOpacity: Double {
+			switch self {
+			case .ready:
+				1.0
+			case .dragging(let (offsetX, maxX)):
+				1.0 - Double(offsetX / maxX)
+			case .end:
+				0.0
+			}
+		}
+	}
+
+	// Private Property
+	@State private var draggableState: DraggableState = .ready
+
+	private func setupView(geometry: GeometryProxy) -> some View {
+		let frame = geometry.frame(in: .global)
+		let width = frame.size.width
+		let height = frame.size.height
+		let drag = DragGesture()
+			.onChanged { drag in
+				let maxX = width - height - thumbnailLeadingTrailingPadding * 2 + thumbnailTopBottomPadding * 2
+				let currentX = drag.translation.width
+				if currentX >= maxX {
+					draggableState = .end(offsetX: maxX)
+					didReachEndAction?(self)
+				} else if currentX <= 0 {
+					draggableState = .dragging(offsetX: 0, maxX: maxX)
+				} else {
+					draggableState = .dragging(offsetX: currentX, maxX: maxX)
+				}
+			}
+			.onEnded(onDragEnded)
+		let sliderCornerRadius = (height - sliderTopBottomPadding * 2) / 2
+		return HStack {
+			ZStack(alignment: .leading, content: {
+				HStack {
+					Text(text)
+						.frame(maxWidth: .infinity)
+						.padding([.leading], textLabelLeadingPadding)
+						.foregroundColor(isDisabled ? .gray : textColor)
+						.opacity(draggableState.textColorOpacity)
+						.animation(draggableState.isReady ? resetAnimation : nil)
+				}
+				.frame(maxWidth: .infinity, maxHeight: .infinity)
+				.background(sliderBackgroundColor)
+				.cornerRadius(sliderCornerRadius)
+				.padding([.top, .bottom], sliderTopBottomPadding)
+
+				Image(systemName: iconName)
+					.foregroundColor(iconColor)
+					.font(.title.weight(.black))
+					.frame(maxWidth: .infinity, maxHeight: .infinity)
+					.aspectRatio(1.0, contentMode: .fit)
+					.background(isDisabled ? .gray : thumbnailColor)
+					.clipShape(Circle())
+					.padding([.top, .bottom], thumbnailTopBottomPadding)
+					.padding([.leading, .trailing], thumbnailLeadingTrailingPadding)
+					.background(thumbnailBackgroundColor)
+					.cornerRadius(sliderCornerRadius)
+					.offset(x: draggableState.offsetX)
+					.animation(draggableState.isReady ? resetAnimation : nil)
+					.gesture(draggableState.reachEnd || isDisabled ? nil : drag)
+			})
+		}
+//		.background(Color.white)
+		.disabled(isDisabled)
+//		.opacity(isDisabled ? 0.5 : 1.0)
+	}
+
+	private func onDragEnded(drag _: DragGesture.Value) {
+		switch draggableState {
+		case .end(_), .dragging:
+			draggableState = .ready
+		case .ready:
+			break
+		}
+	}
+}
